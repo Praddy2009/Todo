604cd0d2eeeb2aab9a1ba8e9a8a6bdfda2e2c757
praddyum verma <praddy2009@gmail.com>
2021-05-19 23:40:01 +0530

    init the project

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+from werkzeug.utils import redirect
+
+app = Flask(__name__)
+
+#Database path-set and initialization
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+db=SQLAlchemy(app)
+
+#Database Schema
+class Todo(db.Model):
+   id= db.Column(db.Integer, primary_key=True)
+   content= db.Column(db.String(200), nullable=False)
+   date_created=db.Column(db.DateTime, default=datetime.utcnow)
+
+   def __repr__(self):
+      return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST','GET'])
+def index():
+   #Check for the type of request
+   if request.method == 'POST':
+      #Logic to add the task
+      task_content= request.form['content']   #task_content=variable
+      new_task=Todo(content=task_content)     #Added input to todo model
+
+      #Push data to database
+      try:
+         db.session.add(new_task)
+         db.session.commit()
+         return redirect('/')
+      except:
+         return 'Eoor: Issue adding the string'
+
+   else:
+      #Reading DB 
+      tasks = Todo.query.order_by(Todo.date_created).all()
+      return render_template("index.html",tasks=tasks)
+
+#Route for delete by id
+@app.route('/delete/<int:id>')
+def delete(id):
+   #lookup for record w.r.t id
+   task_to_delete =Todo.query.get_or_404(id)
+
+   try:
+      db.session.delete(task_to_delete)
+      db.session.commit()
+      return redirect('/')
+   except:
+      return 'There was a problem deleting'
+
+#Route for update by id
+@app.route('/update/<int:id>',methods=['GET','POST'])
+def update(id):
+   task =Todo.query.get_or_404(id)
+   if request.method=='POST':
+      task.content= request.form['content']
+
+      try:
+         db.session.commit()
+         return redirect('/')
+      except:
+         return 'Error Updating'
+   else:
+      return render_template("update.html",task=task)
+
+
+if __name__ == "__main__":
+   app.run(debug=True)
