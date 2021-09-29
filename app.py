50e0d74ee560468ac953ffa70200c8fb720564f0
praddyum verma <praddy2009@gmail.com>
2021-09-29 12:18:55 +0530

    Added API for symbols

--- app.py
+++ app.py
@@ -53,6 +53,12 @@ def delete(id):
    except:
       return 'There was a problem deleting'
 
+#Rout for Symbols
+@app.route('/symbol')
+def symbols():
+   a=('AFN','ARS','AWG','AUD','AZN','BSD','BBD','BYN','BZD','BMD','BOB','BAM','BWP','BGN','BRL','BND','KHR','CAD','KYD','CLP','CNY','COP','CRC','HRK','CUP','CZK','DKK','DOP','XCD','EGP','SVC','EUR','FKP','FJD','GHS','GIP','GTQ','GGP','GYD','HNL','HKD','HUF','ISK','INR','IDR','IRR','IMP','ILS','JMD','JPY','JEP','KZT','KPW','KRW','KGS','LAK','LBP','LRD','MKD','MYR','MUR','MXN','MNT','MZN','NAD','NPR','ANG','NZD','NIO','NGN','NOK','OMR','PKR','PAB','PYG','PEN','PHP','PLN','QAR','RON','RUB','SHP','SAR','RSD','SCR','SGD','SBD','SOS','ZAR','LKR','SEK','CHF','SRD','SYP','TWD','THB','TTD','TRY','TVD','UAH','GBP','USD','UYU','UZS','VEF','VND','YER','ZWD','Lek','؋','$','ƒ','₼','Br','BZ$','$b','KM','P','лв','R$','៛','¥','₡','kn','₱','Kč','kr','RD$','£','€','¢','Q','L','Ft','','Rp','﷼','₪','J$','₩','₭','ден','RM','₨','₮','MT','C$','₦','B/.','Gs','S/.','zł','lei','₽','Дин.','S','R','CHF','NT$','฿','TT$','₴','$U','Bs','₫','Z$')
+   return '{}'.format(a)
+
 #Route for update by id
 @app.route('/update/<int:id>',methods=['GET','POST'])
 def update(id):
